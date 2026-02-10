1dcd60c66a574849c7816157044c7d73f04cd542
Dominik Rybski <d.rybski35@gmail.com>
2026-02-10 11:14:01 +0100

    commit

--- ParkingSystem/obj/Debug/net9.0/ParkingSystem.AssemblyInfo.cs
+++ ParkingSystem/obj/Debug/net9.0/ParkingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b35dd159a70575497e36d960ec2b1cd6c41725f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+790fda53c1f2bc3a8b6d1537e16b9f4b0d5da2a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
